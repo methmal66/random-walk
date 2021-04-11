588b60d04be31b0d443cdb6b6997e284039c080b
Sanuja Methmal <methmal66@yahoo.com>
2021-04-11 08:18:50 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+
+try {
+  require("electron-reloader")(module);
+} catch (_) {}
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("./index.html");
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
---
+++ source/sketch.js
@@ -0,0 +1,5 @@
+function setup() {
+  createCanvas(790, 580);
+}
+
+function draw() {}
