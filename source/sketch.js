b4c3be492f3cf0a3d248643b3c4ebc9ba4872c28
Sanuja Methmal <methmal66@yahoo.com>
2021-04-12 08:25:58 +0530

    random walk works well

--- source/sketch.js
+++ source/sketch.js
@@ -1,5 +1,39 @@
+const getRandomDirection = (stepSize) => {
+  const DIRECTIONS = [
+    { x: stepSize, y: stepSize },
+    { x: -stepSize, y: stepSize },
+    { x: stepSize, y: -stepSize },
+    { x: -stepSize, y: -stepSize },
+  ];
+  return () => {
+    const option = Math.round(3 * Math.random());
+    return DIRECTIONS[option];
+  };
+};
+
+const randomDirection = getRandomDirection(2);
+
+const nextCoordinates = (initialLocation) => {
+  let currentLocation = initialLocation;
+  return () => {
+    const step = randomDirection();
+    const gotoLocation = {
+      x: currentLocation.x + step.x,
+      y: currentLocation.y + step.y,
+    };
+    currentLocation = gotoLocation;
+    return gotoLocation;
+  };
+};
+
+const findNextCoordinates = nextCoordinates({ x: 200, y: 200 });
+
 function setup() {
   createCanvas(790, 580);
+  strokeWeight(3);
 }
 
-function draw() {}
+function draw() {
+  const coords = findNextCoordinates();
+  point(coords.x, coords.y);
+}
